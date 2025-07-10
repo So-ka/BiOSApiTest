b84d5dee256038532f98c5ded159773096255719
Soheib Laichi <soheib.lai@gmail.com>
2025-07-10 01:57:13 +0100

    apiget

--- index.js
+++ index.js
@@ -25,7 +25,32 @@ app.post('/api/v11/users/:userId/sessions', (req, res) => {
 app.post('/api/v11/jobs', requireToken, (req, res) => {
   //controle how jobs are stored
   const newjobid = `job-${Date.now()}`;
-  const job = { ...req.body, JobId: newjobid};
+  const job = {
+              "JobId": newjobid, 
+              "JobPriority": req.body.JobPriority || 0, 
+              "JobName": req.body.JobName, 
+              "JobType": req.body.JobType, 
+              "JobState": "Complete", 
+              "CreatedBy": "admin", 
+              "CreatedAt": "2015-01-19T07:21:09+01:00", 
+              "StartDate": "2015-01-19T07:21:09.33+01:00", 
+              "EndDate": "2015-01-19T07:21:10.087+01:00", 
+              "JobProperties": req.body.JobProperties || [], 
+              "PredecessorJobIds": [ 
+                "7ad5ebe1c3d44429960c9254591cb42a" 
+              ], 
+              "SuccessorJobIds": [], 
+              "ProcessLogs": [ 
+                { 
+                  "End": "2015-01-19T07:21:10.087+01:00", 
+                  "FinishReason": "Completed", 
+                  "ID": "41a151de56fd4e6098286b02c57d15d5", 
+                  "Start": "2015-01-19T07:21:09.33+01:00", 
+                  "ErrorItem": null 
+                } 
+              ], 
+              "UserRights": [] 
+  }
   db.jobs.push(job);
   result = {
     "ItemId": newjobid, 
@@ -36,6 +61,18 @@ app.post('/api/v11/jobs', requireToken, (req, res) => {
   res.status(201).json(result);
 });
 
+app.get('/api/v11/jobs/:jobId', (req, res) => {
+  const jobId = req.params.jobId;
+  const job = jobs.find(j => j.JobId === jobId);
+  if (job) {
+    res.json(job);
+  } else {
+    res.status(404).json({ message: 'Job not found' });
+  }
+});
+
+
+//api
 app.listen(port, () => {
   console.log(`Mock Hamilton API running at http://localhost:${port}`);
 });
